b8517579ac195e7df82d4d74d318323c27c30fe6
Flavien Solt <flsolt@ethz.ch>
2024-07-18 13:13:37 +0200

    Improve sh file

--- docker/rebuild-push.sh
+++ docker/rebuild-push.sh
@@ -5,7 +5,6 @@
 set -e
 IMAGE_TAG=ethcomsec/hybridift:hybridift-artifacts
 echo building $IMAGE_TAG
-docker login registry-1.docker.io
 docker build -f Dockerfile -t $IMAGE_TAG .
 echo "To push image, do:"
 echo "docker push $IMAGE_TAG"
